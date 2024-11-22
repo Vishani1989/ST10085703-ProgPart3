d5abe52f72df90ea066ccf700465e2c11cf327b4
Vishani1989 <126716221+Vishani1989@users.noreply.github.com>
2024-11-22 23:52:50 +0200

    Part 3 Project
    Users work Claims dont work

---
+++ ProgPart3CMCS/ProgPart3CMCS/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ProgPart3CMCS
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ProgPart3CMCS/ProgPart3CMCS/Controllers/ClaimsController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProgPart3CMCS.Models;
+
+namespace ProgPart3CMCS.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        private DatabaseCMCSEntities1 db = new DatabaseCMCSEntities1();
+
+        // GET: Claims
+        public ActionResult Index()
+        {
+            return View(db.Claims.ToList());
+        }
+
+        // GET: Claims/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Claim claim = db.Claims.Find(id);
+            if (claim == null)
+            {
+                return HttpNotFound();
+            }
+            return View(claim);
+        }
+
+        // GET: Claims/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Claims/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ClaimID,UserID,SubmittedDate,StartDate,EndDate,HoursWorked,HourlyRate,DocumentPath,Status")] Claim claim)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Claims.Add(claim);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+                {
+                    foreach (var validationError in ex.EntityValidationErrors)
+                    {
+                        foreach (var error in validationError.ValidationErrors)
+                        {
+                            ModelState.AddModelError("", $"Property: {error.PropertyName}, Error: {error.ErrorMessage}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", $"Unexpected error: {ex.Message}");
+                }
+            }
+
+            return View(claim);
+        }
+
+
+
+        // GET: Claims/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Claim claim = db.Claims.Find(id);
+            if (claim == null)
+            {
+                return HttpNotFound();
+            }
+            return View(claim);
+        }
+
+        // POST: Claims/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ClaimID,SubmittedDate,StartDate,EndDate,HoursWorked,HourlyRate,DocumentPath,Status,UserID")] Claim claim)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(claim).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(claim);
+        }
+
+        // GET: Claims/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Claim claim = db.Claims.Find(id);
+            if (claim == null)
+            {
+                return HttpNotFound();
+            }
+            return View(claim);
+        }
+
+        // POST: Claims/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Claim claim = db.Claims.Find(id);
+            db.Claims.Remove(claim);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
